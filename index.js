6f486d7261c42ddee019287a7a03f7d63ca4835e
Parv Kumar <parvathikumar92@gmail.com>
2017-10-23 21:58:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,11 @@ function driversByRevenue(drivers){
 
   return newDrivers.sort(numSorter)
 }
+
+function driversByName(drivers){
+  let newDrivers = drivers 
+
+  return newDrivers.sort(function(a,b){
+    return a.name.localeCompare(b.name)
+  })
+}
